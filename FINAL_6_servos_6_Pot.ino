3fde999e2c2dc7f6baf3c7a6d9150f5099ef5e7e
Felipe @Marshall <gali1383@marshallschool.org>
2019-10-07 17:36:51 -0500

    iniital code upload"
    '
    ;
    /

--- FINAL_6_servos_6_Pot.ino
+++ FINAL_6_servos_6_Pot.ino
@@ -1,242 +1,195 @@
-/*Code for arm proyect*/
-
 #include <Servo.h>
-
-struct servoData {
-  bool enabled;
-  String fName;
-  byte potPin;
-  byte serPin;
-  byte minP;
-  byte maxP;
-  byte minS;
-  byte maxS;
-  byte potReading;
-  byte prevReading;
-  byte prevReading2;
-};
-
-struct sPosition {
-  byte pinkyF;
-  byte ringF;
-  byte middleF;
-  byte indexF;
-  byte thumbF;
-  byte wrist;
-};
-
-/*
-  mode 1: read data from 10k pot and move servos acordingly
-  mode 2: secuential move
-  mode 3: move by serial (NOT READY)
-  mode 4: move all to constPos
-  mode 5: individual controll. (NOT READY)
-  mode 6: sequential move
-*/
-const byte mode = 6;
-
-//determines whether or not to print debug data to the terminal.
-const bool printPotVals = true;   //prints input data from each potenciometer
-const bool printSerVals = true;   //prints output data to each servo
-const bool printSettings = true;  //prints current servo settings when booting up.
-
-/*time delay used in modes:
-  Sequential mode(time between movements)
-
-*/
-const unsigned int delayTime = 150;
-
-const byte constPos = 50; //int between 0 and 100
-
-const byte numServos = 6; // positive int 0-6, number of servos
-
-/*
-  BETA FEATURE:
-  determines whether to run obsenity filter */
-const bool filterObsenity = TRUE;
-
-servoData handProfile[numServos] {
-  {true, "Pinky", 0,  3, 0, 900, 45, 170, 50, 50, 50 },
-  {true, "ring Finger", 1,  5, 0, 900, 55, 170, 50, 50, 50 },
-  {true, "middle Finger", 2,  6, 0, 900, 40, 155, 50, 50, 50 },
-  {true, "index", 4,  9, 0, 900, 45, 170, 50, 50, 50 },
-  {true, "thumb", 5, 10, 0, 900, 45, 170, 50, 50, 50 },
-  {false, "DISABLED", 3, 11, 0, 900, 45, 170, 50, 50, 50 }
-};
-
-// Creates vector servo objects and creates setting variables.
-Servo servo[numServos];
-
-// defined positions
-sPosition numberOne {0, 0,  0, 90, 90};
-sPosition peace     {0, 0, 90, 90, 90};
-
-
-/**/
-
-
-/*prints and formats data from parameter servoData*/
-void printServoData(servoData servoSetting) {
-  Serial.println("Attached "+servoSetting.fName
-  +" PRange: "+String(servoSetting.minP)+"-"+String(servoSetting.maxP)
-  +" SRange: "+String(servoSetting.minS)+"-"+String(servoSetting.maxS)
-  +" sPin: "+String(servoSetting.serPin)+" pPin: "+String(servoSetting.potPin) );
-}
-
-/*Attaches all the servos to the pins they are connected to and
-  prints debug data if flag is set*/
-void servosAttach() {
-  for (i = 0; i > numServos; i++) {
-    Servo ser;
-    ser.attach(handProfile[i].serPin, handProfile[i].minS, handProfile[i].maxS);
-    servo[i] = ser;
-    if (printSettings) {
-      printServoData(handProfile[i]);
-    }
-  }
-}
-
-/*Reads and averages reading for provided servoData profile*/
-void readPot(&servoData Sser) {
-  if (Sser.enabled) {
-    byte potVal = map(analogRead(Sser.potPin), Sser.minP, Sser.maxP, 0, 100);
-    if (potVal < Sser.minP) {
-      potVal = Sser.minS;
-    } else if (potVal > Sser.maxP) {
-      potVal = Sser.maxS;
-    }
-    if (printPotVals) {
-      Serial.print("p:" + String(potVal) + " ");
-    }
-    Sser.prevReading2 = Sser.prevReading;
-    Sser.prevReading = Sser.potReading;
-    Sser.potReading = (potVal + ser.prevReading + Sser.prevReading2) / 3
-  }
-}
-
-/*updates positions for servos and returns a position value*/
-sPosition readPotPositions() {
-  return sPosition inputPosition{
-    readPot(Ser1),
-    readPot(Ser2),
-    readPot(Ser3),
-    readPot(Ser4),
-    readPot(Ser5)
-  }
-}
-
-/*moves all the servos sequentialy*/
-void sequentialMove(int wtime) {
-  for (int cpos = 0; cpos <= 180; cpos += 20) { // goes from 0 degrees to 180 degrees
-    String Scpos = String(cpos);
-    serialServoReport(Scpos);
-    allservosToPos(cpos, wtime);
-  }
-  for (int cpos = 180; cpos >= 0; cpos -= 20) { // goes from 180 degrees to 0 degrees
-    String Scpos = String(cpos);
-    serialServoReport(Scpos);
-    allservosToPos(cpos, wtime);
-  }
-}
-void setServo(servoData Sser, byte perce) {
-  
-  if (printSerVals) {
-    Serial.print(Sser.fName + ":" + String(serVal) + " ");
-  }
-}
-void moveHand(sPosition) {
-  for (byte i = 0; i <= numServos; i++) {
-    if (handProfile[i].enabled) {
-      setServo(handProfile[i], );
-    }
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void setup() {
-  if (printSerVals || printPotVals) {
-    Serial.begin(9600);
-    Serial.println("serial ready");
-  }
-  if (printSettings) {
-
-  }
-  servosAttach();
-  delay(3000);
-}
-
-
-
-void loop() {
-
-  switch (mode) {
-    //AnalogSet
-    case 1:
-      break;
-
-    //Sequential Move
-    case 2:
-      sequentialMove(delayTime);
-      break;
-
-    //
-    case 3:
-      break;
-
-    // move all to constPos
-    case 4:
-      String Scpos = String(constPos);
-      serialServoReport(Scpos);
-      allservosToPos(constPos, delayTime);
-      break;
-
-    //
-    case 5:
-      break;
-
-    //
-    case 6:
-      Serial.println("in loop sequential start");
-      sequentialMove(delayTime);
-      Serial.println("in loop sequential");
-      break;
-
-    default:
-      Serial.println("Wrong mode");
-      delay(500);
-      break;
-  }
-}
-
-
-
-// Maps a potentiomenter according to the
-// int mapSer(servoData Sser){
-//   int potVal = analogRead(Sser.potPin);
-//   int serVal = map(potVal, Sser.minP, Sser.maxP, Sser.minS, Sser.maxS);
-
-// ser1.attach(3);
-//   ser2.attach(5);
-//   ser3.attach(6);
-//   ser4.attach(9);
-//   ser5.attach(10);
-//   ser6.attach(11);
+//mode 1: read data from 10k pot and move servos acordingly​
+//mode 2: secuential move ​
+//mode 3: move by serial (NOT READY)​
+//mode 4: move all to defined pos.​
+//mode 5: individual controll. (NOT READY)​
+//mode 6: sequential move​
+const byte mode = 6;​
+//determines whether or not to print stuff for ​
+const bool printPotVals = true;​
+const bool printSerVals = true;​
+const bool printSettings = true;​
+​
+int delayTime = 150;​
+const byte constPos = 50; //int between 0 and 100​
+​
+struct sPosition {​
+  byte pinkyF;​
+  byte ringF;​
+  byte middleF;​
+  byte indexF;​
+  byte thumbF;​
+};​
+  ​
+struct servoSetting{​
+  String fName;​
+  int potNum;​
+  int minP;​
+  int maxP;​
+  int minS;​
+  int maxS;​
+}; ​
+​
+// Creates servo objects and creates setting variables.​
+Servo ser1;​
+servoSetting Sser1 = {"Pinky", 0, 0, 900, 45, 170 };​
+Servo ser2;​
+servoSetting Sser2 = {"ring Finger", 1, 0, 900, 55, 170 }; ​
+Servo ser3;​
+servoSetting Sser3 = {"middle Finger", 2, 0, 900, 40, 155 }; ​
+Servo ser4;​
+servoSetting Sser4 = {"index", 4, 0, 900, 45, 170 }; ​
+Servo ser5;​
+servoSetting Sser5 = {"thumb", 5, 0, 900, 45, 170 }; ​
+Servo ser6;​
+servoSetting Sser6 = {"DISABLED", 3, 0, 900, 45, 170 }; ​
+​
+// defined positions​
+struct ​
+​
+// Attaches all the servos to the pins they are connected to. ​
+void servosAttach(){​
+  ser1.attach(3);​
+  ser2.attach(5);​
+  ser3.attach(6);​
+  ser4.attach(9);​
+  ser5.attach(10);​
+  ser6.attach(11);​
+  ​
+  ser1.write(90);​
+  ser2.write(90);​
+  ser3.write(90);​
+  ser4.write(90);-​
+  ser5.write(90);​
+  ser6.write(90);​
+}​
+// Maps a potentiomenter according to the ​
+int mapPot(servoSetting Sser){​
+  int potVal = analogRead(Sser.potNum);​
+  int serVal = map(potVal, Sser.minP, Sser.maxP, Sser.minS, Sser.maxS);​
+  if(serVal<Sser.minS){​
+    serVal=Sser.minS;​
+  }else if(serVal>Sser.maxS){​
+    serVal=Sser.maxS;​
+    }​
+  if(printSerVals){​
+    Serial.print(Sser.fName+":"+String(serVal)+" ");}​
+  if(printPotVals){​
+    Serial.print("p:"+String(potVal)+" ");}​
+  return serVal;​
+}​
+​
+// Takes the input from the analog pin and sets it to the digital output to the servo. Uses maping to ajust the value. ​
+void analogSet(){​
+  s​
+​
+  ​
+  ser1.write(mapPot(Sser1));​
+  ser2.write(mapPot(Sser2));​
+  ser3.write(mapPot(Sser3));​
+  ser4.write(mapPot(Sser4));​
+  ser5.write(mapPot(Sser5));​
+​
+  ser6.write(mapPot(Sser6));​
+  Serial.println();​
+}​
+//moves all the servos sequentialy​
+void sequentialMove(int wtime){​
+  for (int cpos = 0; cpos <= 180; cpos += 20) { // goes from 0 degrees to 180 degrees​
+    String Scpos = String(cpos);​
+    serialServoReport(Scpos);​
+    allservosToPos(cpos, wtime);​
+  }​
+  for (int cpos = 180; cpos >= 0; cpos -= 20) { // goes from 180 degrees to 0 degrees​
+    String Scpos = String(cpos);​
+    serialServoReport(Scpos);​
+    allservosToPos(cpos, wtime);​
+  }​
+}​
+​
+​
+void allservosToPos(int pos, int dtime){​
+  ser1.write(pos);​
+  delay(dtime);   ​
+  ser2.write(pos);​
+  delay(dtime);   ​
+  ser3.write(pos);​
+  delay(dtime);   ​
+  ser4.write(pos);​
+  delay(dtime);   ​
+  ser5.write(pos);​
+  delay(dtime);   ​
+  ser6.write(pos);              // tell servo to go to position in variable 'pos'​
+  delay(dtime);                       // waits 15ms for the servo to reach the position​
+}​
+​
+void serialServoReport(String serSPos){​
+  String text1 = "Servos Pos is: ";​
+  String linb =  "\n";​
+   ​
+  String serOutAr = text1+serSPos+linb ;​
+  Serial.print(serOutAr);​
+}​
+​
+void reportServos(){​
+  Serial.println("Pot Vals 1: "+String(analogRead(Sser1.potNum))+" 2: "+String(analogRead(Sser2.potNum))+" 3: "+String(analogRead(Sser3.potNum))+" 4: "+String(analogRead(Sser4.potNum))+" 5: "+String(analogRead(Sser5.potNum))+" 6: "+String(analogRead(Sser6.potNum)));​
+  Serial.println("Servo Pos 1: "+String(mapPot(Sser1))+" 2: "+String(mapPot(Sser2))+" 3: "+String(mapPot(Sser3))+" 4: "+String(mapPot(Sser4))+" 5: "+String(mapPot(Sser5))+" 6: "+String(mapPot(Sser6)));​
+  }​
+void setup() {​
+  if(printSerVals||printPotVals){​
+    Serial.begin(9600);​
+    Serial.println("serial ready");​
+  }​
+  if(printSettings){​
+    ​
+  }​
+  servosAttach();​
+  delay(3000);​
+}​
+​
+​
+​
+void loop() {​
+  analogSet();​
+​
+  delay(100);​
+  ​
+  switch (mode) {​
+    //AnalogSet​
+    case 1:​
+    break;​
+    ​
+    //Sequential Move​
+    case 2:​
+    sequentialMove(delayTime);​
+    break;​
+    ​
+    //​
+    case 3:​
+    break;​
+    ​
+    //​
+    case 4:​
+    String Scpos = String(constPos);​
+    serialServoReport(Scpos);​
+    allservosToPos(constPos, delayTime);​
+    break;​
+​
+    //​
+    case 5:​
+    break;​
+​
+    //​
+    case 6:​
+    Serial.println("in loop sequential start");​
+    sequentialMove(delayTime);​
+    Serial.println("in loop sequential");​
+    break;​
+    ​
+    default:​
+    Serial.println("Wrong mode");​
+    delay(500);​
+    break;​
+  }​
+}​
