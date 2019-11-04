866c6815ec2a7523e20cb3e8562a14c7489aee77
Felig2013 <felipht@hotmail.com>
2019-11-04 15:50:35 -0600

    i typed I like github

--- HAND-Proyect-.ino
+++ HAND-Proyect-.ino
@@ -8,24 +8,9 @@
 //somehow relate thresholds with cycles required for sleep
 //Label constants as such 
 // change constants to mayus to use standar notation. 
+// i like github
+#include "header.h"
 
-
-/*Custom data structure to replace the default arduino servo
-  library with a more bare-metal, more efficient deployment
-  that is more light in with the EPROM and enhances longevity*/
-struct servoData {
-  bool enabled;
-  String fName;
-  byte potPin;
-  byte serPin;
-  int minP;
-  int maxP;
-  int minS;
-  int maxS;
-  byte potReading;
-  byte prevReading;
-  byte prevReading2;
-};
 
 /*Custom data structure, used to store a particular position for
   the hand.
---
+++ methods.ino
@@ -0,0 +1,17 @@
+
+/*Custom data structure to replace the default arduino servo
+  library with a more bare-metal, more efficient deployment
+  that is more light in with the EPROM and enhances longevity*/
+struct servoData {
+  bool enabled;
+  String fName;
+  byte potPin;
+  byte serPin;
+  int minP;
+  int maxP;
+  int minS;
+  int maxS;
+  byte potReading;
+  byte prevReading;
+  byte prevReading2;
+};
